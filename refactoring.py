e4882d3fbaa23b1f51a4c5999c804139a6f43a5e
Seunghyun-Kang <kiseyno@gmail.com>
2022-09-29 17:11:13 +0900

    Complete KIS class refactoring

--- refactoring.py
+++ refactoring.py
@@ -21,11 +21,13 @@ import holidays
 from abc import ABC, abstractmethod
     
 class AutoTrader(ABC):
-    def __init__(self):
+    def __init__(self, country):
         print("AutoTrader init()")
         self.kakao = kakao.Kakao()
         self.conn = None
+        self.country = country
         self.holidays = []
+        self.ticker = {}
         self.signals = []
         self.company = {}
         self.weekday = datetime.today().weekday()
@@ -59,13 +61,27 @@ class AutoTrader(ABC):
     def login(self):
         pass
 
-    @abstractmethod
-    def getSignalDate(self):
-        pass
-
-    @abstractmethod
     def isWorking(self):
-        pass
+        if self.weekday == 5 or self.weekday == 6:
+            self.printlog("--오늘은 주말--")
+            return False
+
+        if self.country == 'KR':
+            _time = datetime.now()
+            for red_day in self.holidays:
+                if _time.month == red_day.month and _time.day == red_day.day:
+                    self.printlog("--오늘은 한국 휴일--")
+                    return False
+
+        elif self.country == 'US':
+            for (date, name) in holidays.UnitedStates(years=_time.year).items():
+                if date.strftime("%Y-%m-%d") == datetime.today().strftime("%Y-%m-%d"):
+                    self.printlog("--오늘은 미국 휴일--")
+                    return False
+        
+        days = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+        self.printlog(f"--오늘은 {days[self.weekday]}--")
+        return True
 
     @abstractmethod
     def getExceptionItemList(self):
@@ -83,6 +99,27 @@ class AutoTrader(ABC):
     def getHoldings(self):
         pass 
 
+    def setHolidays(self):
+        if self.country == 'KR':
+            kr_holidays = pytimekr.holidays(year=datetime.now().year)
+            red_days_chuseok = pytimekr.red_days(pytimekr.chuseok(year=datetime.now().year))
+            red_days_lunar_newyear = pytimekr.red_days(pytimekr.lunar_newyear(year=datetime.now().year))
+            return kr_holidays + red_days_lunar_newyear + red_days_chuseok
+
+        elif self.country == 'US':
+            return list(holidays.UnitedStates(years=datetime.now().year).keys())
+
+    def getSignalDate(self):
+        if self.country == 'KR':
+            _time = datetime.today() - timedelta(1)
+        elif self.country == 'US':
+            _time = datetime.utcnow() - timedelta(1)
+
+        while self.isHoliday(_time) == True:
+            _time = _time - timedelta(1)
+        
+        return _time.strftime("%Y-%m-%d")
+
     def printlog(self, content):
         print(content)
         self.f.write(f"{content}")
@@ -98,9 +135,9 @@ class AutoTrader(ABC):
             return False
 
 class Creon(AutoTrader):
-    def __init__(self):
-        super().__init__()
-        self.holidays = self.setHolidays()
+    def __init__(self,country):
+        super().__init__(country)
+        self.holidays = self.setHolidays(country)
         
     def login(self):
         os.system('taskkill /IM coStarter*  /F  /T')
@@ -133,20 +170,6 @@ class Creon(AutoTrader):
                 company = companyPD.values[pos][1]
                 self.company[code] = company
 
-    def setHolidays(self):
-        kr_holidays = pytimekr.holidays(year=datetime.now().year)
-        red_days_chuseok = pytimekr.red_days(pytimekr.chuseok(year=datetime.now().year))
-        red_days_lunar_newyear = pytimekr.red_days(pytimekr.lunar_newyear(year=datetime.now().year))
-        
-        return kr_holidays + red_days_lunar_newyear + red_days_chuseok
-
-    def getSignalDate(self):
-        _time = datetime.today() - timedelta(1)
-        
-        while self.isHoliday(_time) == True:
-            _time = _time - timedelta(1)
-        
-        return _time.strftime("%Y-%m-%d")
 
     def getExceptionItemList(self):
         breakstocks = []
@@ -160,22 +183,6 @@ class Creon(AutoTrader):
 
         return breakstocks
         
-    def isWorking(self):
-        _time = datetime.now()
-
-        for red_day in self.holidays:
-            if _time.month == red_day.month and _time.day == red_day.day:
-                self.printlog("--오늘은 한국 휴일--")
-                return False
-
-        if self.weekday == 5 or self.weekday == 6:
-            self.printlog("--오늘은 한국 주말--")
-            return False
-        else:
-            days = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
-            self.printlog(f"--오늘은 한국 {days[self.weekday]}--")
-            return True
-
     def buy(self, code, num):
         self.creon.buy(code, num, 0)
         time.sleep(1.5)
@@ -195,6 +202,77 @@ class Creon(AutoTrader):
              data['profit_rate'] = item['수익률']
              data['name'] = item['종목명']
              holdings.append(data)
+
+        return holdings
+ 
+    def callback(self, item):
+        print(f"callbakc recieved:: {item}")
+        _hash = item['주문번호']
+        _date = datetime.today().strftime("%Y-%m-%d")
+        _type = "buy"
+        code = item['종목코드'].replace("A","")
+
+        if item['매매구분코드'] == "1" or item['매매구분코드'] == 1:
+            _type = "sell"
+        if item['체결가격'] == 0:
+            return
+
+        if _type == "buy":
+            self.printlog(f"매수 체결 완료: {self.company[code]}, 체결수량 {item['체결수량']}, 체결 가격 {format(item['체결가격'], ',')}\n")
+        else:
+            self.printlog(f"매도 체결 완료: {self.company[code]}, 체결수량 {item['체결수량']}, 체결 가격 {format(item['체결가격'], ',')}\n")
+
+class Kis(AutoTrader):
+    def __init__(self, country):
+        super().__init__(country)
+        self.holidays = self.setHolidays(country)
+        
+    def login(self):
+        self.kis = kis.KIS()
+
+    def getCompanyList(self, table_name):
+        with self.conn.cursor() as curs :
+            sql = f"select code, company, ticker from {table_name}"
+            curs.execute(sql) 
+            companyPD = pd.DataFrame(curs.fetchall())
+            
+            for pos in range(len(companyPD)):
+                code = companyPD.values[pos][0]
+                self.company[code] = companyPD.values[pos][1]
+                self.ticker[code] = companyPD.values[pos][2]
+
+    def getExceptionItemList(self):
+        pass
+        
+    def buy(self, code, num, price):
+        ticker = ''
+        if self.ticker[code] == 'NASDAQ':
+            ticker = 'NASD'
+        else:
+            ticker = self.ticker[code]
+        self.kis.do_buy(ticker, code, num, price, prd_code="01", order_type="00")
+
+    def sell(self, code, num, price):
+        ticker = ''
+        if self.ticker[code] == 'NASDAQ':
+            ticker = 'NASD'
+        else:
+            ticker = self.ticker[code]
+        self.kis.do_sell(ticker, code, num, price, prd_code="01", order_type="00")
+            
+    def getHoldings(self):
+        holdings = []
+        items = self.kis.get_acct_balance()
+
+        for idx in range(len(items)):
+            data = {}
+            data['code'] = items.코드.values[idx]
+            data['name'] = items.종목명.values[idx]
+            data['profit'] = float(items.실현손익.values[idx])
+            data['profit_rate'] = float(items.수익률.values[idx])
+            data['price'] = round(float(items.가격.values[idx]) * 0.98, 2)
+            data['possible_sell'] = float(items.매도가능수량.values[idx])
+            holdings.append(data)
 
         return holdings
  
