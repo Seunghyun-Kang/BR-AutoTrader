6b10d6a2c17d4324ed113916f25cbea2d0148295
Seunghyun-Kang <kiseyno@gmail.com>
2022-12-15 00:00:04 +0900

    Update trade creon module

--- test_creon_module.py
+++ test_creon_module.py
@@ -7,19 +7,9 @@ from module import trade_creon
 
 a  = trade_creon.CreonTradeModule()
 
-a.connect_api()
-a.connect_database()
-a.set_companies()
-
-signal_day = a.get_signal_day()
-signal_list = a.get_signals_from_core(signal_day)
-
-holding_stocks = a.get_holding_stocks()
-account_money = a.get_account_money()
-
-print(signal_list)
+print(a.signal_list)
 print("--------------------------------------------------")
-print(holding_stocks)
+print(a.holding_stocks)
 print("--------------------------------------------------")
-print(account_money)
+print(a.account_money)
 #57506623
